216bdf38ca6c3b802a952fa131a6b7741e868015
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-08 10:27:49 +0700

    Link system stdc++ for Zygisk API

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -5,5 +5,5 @@ LOCAL_MODULE := wastatushd
 LOCAL_SRC_FILES := module.cpp
 LOCAL_CFLAGS := -Wall -Wextra -Werror
 LOCAL_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS := -llog -lstdc++
 include $(BUILD_SHARED_LIBRARY)
