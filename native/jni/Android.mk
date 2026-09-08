0b0240691f94c2168de02021ad4ac240d8b81562
Ruzakj <93422529+Ruzakj@users.noreply.github.com>
2026-09-08 10:25:11 +0700

    Add native Android.mk

---
+++ native/jni/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := wastatushd
+LOCAL_SRC_FILES := module.cpp
+LOCAL_CFLAGS := -Wall -Wextra -Werror
+LOCAL_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti
+LOCAL_LDLIBS := -llog
+include $(BUILD_SHARED_LIBRARY)
